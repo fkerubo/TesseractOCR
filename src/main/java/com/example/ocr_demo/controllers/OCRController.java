862cf046197a111c43260cc9a7f9ee209e5fc265
Faith Kerubo <kengabifaith2002@gmail.com>
2026-02-18 19:47:47 +0300

    comments

--- src/main/java/com/example/ocr_demo/controllers/OCRController.java
+++ src/main/java/com/example/ocr_demo/controllers/OCRController.java
@@ -9,17 +9,17 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
-@RestController
-@RequestMapping("/ocr")
+@RestController //This class handles HTTP requests and returns JSON/text responses.
+@RequestMapping("/ocr") //All endpoints in this controller will start with:http://localhost:8080/ocr
 public class OCRController {
-
+//Dependency Injection To ensure the flow is Controller ➝ Service ➝ Tesseract ➝ Extract Text
     private final OCRService ocrService;
 
     public OCRController(OCRService ocrService) {
         this.ocrService = ocrService;
     }
 
-    @PostMapping("/scan")
+    @PostMapping("/scan")//Main Endpoint
     public ResponseEntity<String> scanId(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().body("No file uploaded");
@@ -38,16 +38,18 @@ public class OCRController {
             tempFile = File.createTempFile("id-", ext);
             file.transferTo(tempFile);
 
-            // Run OCR
+            // Run OCR (This calls my service layer:)
             String text = ocrService.extractText(tempFile);
 
-            return ResponseEntity.ok(text);
+            return ResponseEntity.ok(text);//Returning the Result
 
-        } catch (IOException ioEx) {
+        } catch (IOException ioEx) //input output exception eg File cannot be created
+        {
             ioEx.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("File handling error: " + ioEx.getMessage());
-        } catch (Exception ex) {
+        } catch (Exception ex) //captures any other exception that's not IO eg server crash
+        {
             ex.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("OCR processing error: " + ex.getMessage());
